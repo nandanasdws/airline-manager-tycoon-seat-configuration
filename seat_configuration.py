c1100fba1111992d92a0b2599684cafe0934fd92
nandanasdws <109350433+nandanasdws@users.noreply.github.com>
2025-12-04 21:14:26 +0700

    uniforming variables

--- seat_configuration.py
+++ seat_configuration.py
@@ -12,17 +12,17 @@ airplane_name = input("Input Airplane Name: ")
 route = input("Input Your Route: ")
 
 #Input the maximum capacity for the airplane
-E_seat = int(input("Input Maximum Economy Seat: "))
-B_seat = int(input("Input Maximum Business Seat: "))
-F_seat = int(input("Input Maximum First Seat: "))
-Max_Seat = E_seat
-print("Max Capacity: ", Max_Seat)
+e_seat = int(input("Input Maximum Economy Seat: "))
+b_seat = int(input("Input Maximum Business Seat: "))
+f_seat = int(input("Input Maximum First Seat: "))
+max_seat = e_seat
+print("Max Capacity: ", max_seat)
 
 #Input the ticket price
 print("-INPUT TICKET PRICE-")
-E_Ticket = int(input("E Ticket ($): "))
-B_Ticket = int(input("B Ticket ($): "))
-F_Ticket = int(input("F Ticket ($): "))
+e_Ticket = int(input("E Ticket ($): "))
+b_Ticket = int(input("B Ticket ($): "))
+f_Ticket = int(input("F Ticket ($): "))
 
 #Input how many simulation needed
 simulation = 1000
@@ -35,17 +35,22 @@ result = []
 while valid < simulation:
 
 #Generate random seat capacity for Business and First class
-    F = random.randint(1,F_seat)
-    B = random.randint(1,B_seat)
+#I prioritize B and F seat first than try to calculate the E seat because it has the most space of all
+    f = random.randint(1,f_seat)
+    b = random.randint(1,b_seat)
+
 #Generate the Economy Seat
-    E = math.ceil(Max_Seat - (1.849617185 * B) - (4.497744588 * F))
+#The formula is based on the average seat gap between each airplane
+    e = math.ceil(max_seat - (1.849617185 * b) - (4.497744588 * f))
+
 #If the E < 0 it will redo again
-    if E > 0:
+    if e > 0:
         valid += 1
 #Calculate the total revenue for each option
-        revenue = (E * E_Ticket) + (B * B_Ticket) + (F * F_Ticket)
 
-        result.append((E, B, F, revenue))
+        revenue = (e * e_Ticket) + (b * b_Ticket) + (f * f_Ticket)
+
+        result.append((e, b, f, revenue))
         
 #Showing the top 5 option
 df = pd.DataFrame(result, columns=["E", "B", "F", "Revenue"])
